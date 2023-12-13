d8547bedf520eeabae0346dcf42281e422e02b7e
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2023-12-13 18:25:43 -0300

    atualização

--- cards.js
+++ cards.js
@@ -1,13 +1,18 @@
 let listaProjetos = [
   {
-      "video": "./video/VdeoCloneNubank.mp4",
-      "nome": "Nubank",
-      "descricao": " Desenvolvi este projeto com o objetivo de criar uma reprodução fiel da interface principal do Nubank, focando principalmente no aprimoramento das minhas habilidades no frontend. Utilizei JavaScript puro e Node.js para interação e criação dinâmica de elementos, além de explorar a inspeção detalhada dos mesmos. A incorporação de frameworks como Bootstrap e Sass contribuiu significativamente para a melhoria da experiência de desenvolvimento. Este projeto é autoral e foi desenvolvido de forma autônoma, refletindo o meu comprometimento e dedicação ao aprimoramento constante das minhas habilidades.",
-  }
+    "video": "./video/VdeoCloneNubank.mp4",
+    "nome": "Clone Interface Nubank",
+    "descricao": " Desenvolvi este projeto com o objetivo de criar uma reprodução fiel da interface principal do Nubank, focando principalmente no aprimoramento das minhas habilidades no frontend. Utilizei JavaScript puro e Node.js para interação e criação dinâmica de elementos, além de explorar a inspeção detalhada dos mesmos. A incorporação de frameworks como Bootstrap e Sass contribuiu significativamente para a melhoria da experiência de desenvolvimento. Este projeto é autoral e foi desenvolvido de forma autônoma, refletindo o meu comprometimento e dedicação ao aprimoramento constante das minhas habilidades.",
+  },
+  {
+    "video": "./video/React.mp4",
+    "nome": "Gerenciador de Projetos",
+    "descricao": " O projeto tem como objetivo principal um gerenciador de projetos, foi porduzido com react e node.js, para se trabalhar com um backend fake.Projeto responviso.  O que mais me motivou a escolhe-lo foi a funcionalidade de gestão de custos, que permite a diminuição e soma dos valores associados a cada serviço adicionado no projeto, proporcionando um controle efetivo do custo total do mesmo. Essa abordagem prática enriqueceu minha compreensão sobre a importância da gestão financeira em projetos de desenvolvimento.Adicionalmente, incluí de forma autônoma uma seção dedicada às tarefas no projeto, visando aprimorar ainda mais o uso de componentes em React. Essa adição permitiu- me consolidar os conhecimentos adquiridos e aplicar, de maneira prática, os conceitos aprendidos durante o desenvolvimento do projeto.Além disso, explorei a implementação de operações CRUD para garantir a eficácia na manipulação de dados.Projeto hoje está rodando localmente, fico a disposição para compartilha - lo, e os comando estaram no github."
+}
 ]
 // criando a estrutura de repetição
 
-listaProjetos.map((projeto, posicao)=>{//parametros (PROJETO, POSICAO) => vão receber ..
+listaProjetos.map((projeto, posicao) => {//parametros (PROJETO, POSICAO) => vão receber ..
   let cardProjetos = document.getElementById("cards");
   cardProjetos.innerHTML += `
   <div class="col-md-6 mb-3 ">
@@ -23,14 +28,14 @@ listaProjetos.map((projeto, posicao)=>{//parametros (PROJETO, POSICAO) => vão r
   `
 })
 
-function zoomImagem (posicao){//vai verificar qual é a posição que a função onliclick está mandando
+function zoomImagem(posicao) {//vai verificar qual é a posição que a função onliclick está mandando
   let selecaoCards = listaProjetos[posicao];
   document.getElementById("nomeProjeto").innerHTML = selecaoCards.nome;
   document.getElementById("descricaoProjeto").innerHTML = selecaoCards.descricao
-  
+
 
   new bootstrap.Modal("#zoom").show(); // ativa modals
-  
+
 }
 
 
@@ -38,7 +43,7 @@ function zoomImagem (posicao){//vai verificar qual é a posição que a função
 //add (feedback)
 //firebase
 //LEMBRAR! DOM usado para garantir que o script JavaScript seja executado somente quando todo o conteúdo HTML da página foi totalmente carregado
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('user-form');
   const feedbackInput = document.getElementById('feedback');
   const userList = document.getElementById('user-list');
@@ -57,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const database = firebase.database();
 
-  form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
     const name = feedbackInput.value;
 
@@ -65,13 +70,13 @@ document.addEventListener('DOMContentLoaded', function() {
     database.ref('usuarios').push({
       name: name
     })
-    .then(() => {
-      const listItem = document.createElement('li');
-      listItem.textContent = name;
-      userList.appendChild(listItem);
-      feedbackInput.value = '';
-    })
-    .catch(error => console.error('Erro:', error));
+      .then(() => {
+        const listItem = document.createElement('li');
+        listItem.textContent = name;
+        userList.appendChild(listItem);
+        feedbackInput.value = '';
+      })
+      .catch(error => console.error('Erro:', error));
   });
 });
 document.addEventListener('DOMContentLoaded', () => {
@@ -92,8 +97,8 @@ document.addEventListener('DOMContentLoaded', () => {
           },
           body: JSON.stringify({ name }),
         });
-    
-        if  (response.status >= 200 && response.status < 300){
+
+        if (response.status >= 200 && response.status < 300) {
           // O feedback foi adicionado com sucesso ao banco de dados
           alert("Feedback adicionado com sucesso")
           console.log('Feedback adicionado com sucesso:', name);
@@ -104,14 +109,14 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Erro durante a requisição:', error);
       }
     }
-    
+
     feedbackInput.value = ''; // Limpa o campo de entrada (feedback)
   });
 });
 
 
 //eventos para timeline + setas
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const timeline = document.getElementById('timeline');
   const arrowLeft = document.querySelector('.arrow-left');
   const arrowRight = document.querySelector('.arrow-right');
@@ -122,18 +127,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const events = [
     { imgSrc: './img/html.png' },
     { imgSrc: './img/css.png' },
-    { imgSrc: './img/bootstrap.png'},
+    { imgSrc: './img/bootstrap.png' },
     { imgSrc: './img/sass.png' },
     { imgSrc: './img/js.png' },
     { imgSrc: './img/node.js.png' },
     { imgSrc: './img/react.png' },
     { imgSrc: './img/kotlin.png' },
-   
 
-   
+
+
     // Adicione mais eventos conforme necessário
   ];
-   //recuperando dados da const events, e add via js de forma dinâmica
+  //recuperando dados da const events, e add via js de forma dinâmica
   events.forEach(event => {
     const eventElement = document.createElement('div');
     eventElement.classList.add('event');
@@ -142,13 +147,13 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Manipulação de setas
-  arrowLeft.addEventListener('click', function() {
-    scrollAmount -= 200; 
+  arrowLeft.addEventListener('click', function () {
+    scrollAmount -= 200;
     timeline.scrollLeft = scrollAmount;
   });
 
-  arrowRight.addEventListener('click', function() {
-    scrollAmount += 200; 
+  arrowRight.addEventListener('click', function () {
+    scrollAmount += 200;
     timeline.scrollLeft = scrollAmount;
   });
 });
